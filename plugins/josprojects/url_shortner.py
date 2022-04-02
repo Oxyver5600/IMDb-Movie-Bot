a173d2d17db710884a19e564163b73563536f61c
Oxyver5600 <89983756+Oxyver5600@users.noreply.github.com>
2022-04-02 10:42:12 +0530

    Update url_shortner.py

--- plugins/josprojects/url_shortner.py
+++ plugins/josprojects/url_shortner.py
@@ -27,7 +27,7 @@ GPLINKS_API = os.environ.get("GPLINKS_API", "008ccaedd6061ad1948838f410947603de9
 
 reply_markup = InlineKeyboardMarkup(
         [[
-        InlineKeyboardButton(text='join projects channel', url='https://telegram.me/josprojects')
+        InlineKeyboardButton(text='join Support channel', url='https://telegram.me/Oxyver_Support_Channel')
         ]]
     )
 
@@ -190,7 +190,7 @@ async def short(link):
     
     # Send the text
     try:
-        shorten_urls += "\n\nmade by @josprojects | @jospsupport 🔥"
+        shorten_urls += "\n\nmade by @Oxyver_Owner | @Oxyver_Owner 🔥"
         return shorten_urls
     except Exception as error:
         return error
